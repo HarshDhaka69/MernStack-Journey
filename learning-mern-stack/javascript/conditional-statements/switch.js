c55c7812b813029bdd4df3dd2e57319a81bcff3d
HarshDhaka69 <itsharshx432@gmail.com>
2026-07-28 00:49:43 +0530

    feat(js): practice JavaScript conditional statements and switch

---
+++ learning-mern-stack/javascript/conditional-statements/switch.js
@@ -0,0 +1,27 @@
+// Switch Condition
+
+// In switch, strict equality (===) is used for comparison.
+
+let marks = 90;
+
+switch (marks) {
+    case 90:
+        console.log("A+");
+        break;
+
+    case 80:
+        console.log("A");
+        break;
+
+    case 70:
+        console.log("B");
+        break;
+
+    case 60:
+        console.log("C");
+        break;
+
+    default:
+        console.log("Try To Improve Yourself");
+        break;
+}
