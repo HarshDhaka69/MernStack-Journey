905284b033305ea7905bf2285d765d2fb89024a3
HarshDhaka69 <ItsHarshx432@gmail.com>
2026-08-04 14:05:21 +0530

    fix(js): resolve variable redeclaration bug in if-else.js
    Each example now lives in its own block scope so the file can actually
    be run end-to-end with Node instead of throwing a SyntaxError on the
    second 'let age' declaration.

--- learning-mern-stack/javascript/conditional-statements/if-else.js
+++ learning-mern-stack/javascript/conditional-statements/if-else.js
@@ -1,53 +1,49 @@
-// Normal Script
-
-let age = 12;
-
-console.log("You Can Vote");
-console.log("You Can't Vote");
-
 // IF Condition
+{
+    let age = 12;
 
-let age = 12;
-
-if (age >= 18) {
-    console.log("You Can Vote")
+    if (age >= 18) {
+        console.log("You Can Vote");
+    } else {
+        console.log("You Can't Vote");
+    }
 }
 
-console.log("You Can't Vote");
-
 // IF ELSE Condition
+{
+    let age = 18;
 
-let age = 18;
-
-if (age >= 18) {
-    console.log("You Can Vote")
-} else {
-    console.log("You Can't Vote");
+    if (age >= 18) {
+        console.log("You Can Vote");
+    } else {
+        console.log("You Can't Vote");
+    }
 }
 
 // Nested If Condition
+{
+    let age = 18;
 
-let age = 18;
+    if (age >= 18) {
+        if (age >= 60) {
+            console.log("You Are Senior Citizen. You Can Vote Online");
+        }
 
-if (age >= 18) {
-    if (age >= 60) {
-        console.log("You Are Senior Citizen. You Can Vote Online");
+        console.log("You Can Vote");
+    } else {
+        console.log("You Can't Vote");
     }
-
-    console.log("You Can Vote");
-} else {
-    console.log("You Can't Vote");
 }
 
-
 // Ladder Condition ( ELSE IF )
+{
+    let age = 18;
 
-let age = 18;
-
-if (age >= 60) {
-    console.log("You Are Senior Citizen You Can Vote Online");
-}else if (age >= 18) { 
-    console.log("You Can Vote"); 
-}else {
-    console.log("You Can't Vote");
+    if (age >= 60) {
+        console.log("You Are Senior Citizen You Can Vote Online");
+    } else if (age >= 18) {
+        console.log("You Can Vote");
+    } else {
+        console.log("You Can't Vote");
+    }
 }
